40c057314e2fa0c195ebc3f21b28f98f4e6f2a9f
Javier Godoy <jack@gaulatti.com>
2026-08-24 01:25:11 -0400

    fix(spritz): authenticate metrics before WordPress boot

--- metrics.php
+++ metrics.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once '/var/www/html/wordpress/wp-load.php';
+require_once '/var/www/html/wordpress/wp-content/mu-plugins/lib/spritz-metrics.php';
 
 if (($_SERVER['REQUEST_METHOD'] ?? 'GET') !== 'GET') {
     http_response_code(405);
@@ -8,13 +8,26 @@ if (($_SERVER['REQUEST_METHOD'] ?? 'GET') !== 'GET') {
     exit;
 }
 
-$expected = (string) (defined('METRICS_TOKEN') ? METRICS_TOKEN : getenv('METRICS_TOKEN'));
-$authorization_status = spritz_metrics_authorization_status($expected, $_SERVER);
+$request_server = $_SERVER;
+if (function_exists('getallheaders')) {
+    foreach (getallheaders() as $name => $value) {
+        if (strcasecmp((string) $name, 'Authorization') === 0) {
+            $request_server['HTTP_AUTHORIZATION'] = (string) $value;
+        }
+        if (strcasecmp((string) $name, 'X-Spritz-Metrics-Token') === 0) {
+            $request_server['HTTP_X_SPRITZ_METRICS_TOKEN'] = (string) $value;
+        }
+    }
+}
+
+$expected = (string) getenv('METRICS_TOKEN');
+$authorization_status = spritz_metrics_authorization_status($expected, $request_server);
 if ($authorization_status !== 200) {
     http_response_code($authorization_status);
     exit;
 }
 
+require_once '/var/www/html/wordpress/wp-load.php';
 spritz_metrics_refresh_translation_queue();
 header('Content-Type: text/plain; version=0.0.4; charset=utf-8');
 header('Cache-Control: no-store');
